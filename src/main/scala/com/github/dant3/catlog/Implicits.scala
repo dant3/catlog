1a3656204db414253a7c6b17f9619400d7eb9374
Vyacheslav Blinov <vblinov@unison.com>
2015-07-17 16:34:32 +0300

    Implicit TextGuiThread => Executor

--- src/main/scala/com/github/dant3/catlog/Implicits.scala
+++ src/main/scala/com/github/dant3/catlog/Implicits.scala
@@ -1,9 +1,16 @@
 package com.github.dant3.catlog
 
+import java.util.concurrent.Executor
+
+import com.github.dant3.catlog.util.GuiThreadExecutor
+import com.googlecode.lanterna.gui2.TextGUIThread
+
 import scala.language.implicitConversions
 
 trait Implicits {
   implicit def toRunnable(block: ⇒ Any):Runnable = new Runnable {
     override def run(): Unit = block
   }
+
+  implicit def guiThreadToExecutor(guiThread:TextGUIThread):Executor = new GuiThreadExecutor(guiThread)
 }
--- src/main/scala/com/github/dant3/catlog/util/GuiThreadExecutor.scala
+++ src/main/scala/com/github/dant3/catlog/util/GuiThreadExecutor.scala
@@ -1,11 +1,11 @@
 package com.github.dant3.catlog.util
 
-import com.googlecode.lanterna.gui2.TextGUI
+import com.googlecode.lanterna.gui2.{TextGUIThread, TextGUI}
 
 import scala.concurrent.ExecutionContextExecutor
 
-class GuiThreadExecutor(gui:TextGUI) extends ExecutionContextExecutor {
-  private val guiThread = gui.getGUIThread
+class GuiThreadExecutor(guiThread:TextGUIThread) extends ExecutionContextExecutor {
+  def this(gui:TextGUI) = this(gui.getGUIThread)
 
   override def reportFailure(cause: Throwable): Unit = ???
   override def execute(command: Runnable): Unit = if (Thread.currentThread() == guiThread.getThread) {
