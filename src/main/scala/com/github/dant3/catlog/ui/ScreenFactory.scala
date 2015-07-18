a1406d120ba9a8f5fa29c1d0ba8f46e3d8cb5a19
Vyacheslav Blinov <vblinov@unison.com>
2015-07-18 12:54:50 +0300

    SwingTerminal doesn't finishing process workaround

--- src/main/scala/com/github/dant3/catlog/ui/ScreenFactory.scala
+++ src/main/scala/com/github/dant3/catlog/ui/ScreenFactory.scala
@@ -1,9 +1,12 @@
 package com.github.dant3.catlog.ui
 
+import java.awt.event.{WindowEvent, WindowAdapter}
 import java.io.IOException
+import javax.swing.JFrame
 
-import com.googlecode.lanterna.screen.{TerminalScreen, Screen}
-import com.googlecode.lanterna.terminal.{Terminal, DefaultTerminalFactory}
+import com.googlecode.lanterna.screen.{Screen, TerminalScreen}
+import com.googlecode.lanterna.terminal.swing.SwingTerminalFrame
+import com.googlecode.lanterna.terminal.{DefaultTerminalFactory, Terminal}
 
 class ScreenFactory {
   private val terminalFactory = new DefaultTerminalFactory()
@@ -16,6 +19,21 @@ class ScreenFactory {
   }
 
   private def createTerminal(useSwing:Boolean):Terminal = {
-    terminalFactory.setSuppressSwingTerminalFrame(!useSwing).createTerminal()
+    tuneSwingWindow(terminalFactory.setSuppressSwingTerminalFrame(!useSwing).createTerminal())
+  }
+
+  private def tuneSwingWindow(terminal: Terminal) = terminal match {
+    case swingFrame: SwingTerminalFrame ⇒
+      swingFrame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE)
+      swingFrame.addWindowListener(new WindowAdapter {
+        override def windowClosing(event:WindowEvent): Unit = {
+          // HACK! ScreenFactory shouldn't be aware of GUI internals, screen and windows
+          import scala.collection.JavaConversions._
+          GUI.gui.getWindows.foreach(_.close())
+          GUI.screen.stopScreen()
+        }
+      })
+      swingFrame
+    case anythingElse ⇒ anythingElse
   }
 }
